528adca5595be65f6468145bf9c2d234908f1c97
TommySpecs . <tommybevell@gmail.com>
2024-03-13 22:55:25 +0000

    Split game into chapters and input story line into each chapter, started game intro and run.py main function

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import a_intro as intro
+
+
+
+if __name__ == "__main__":
+    print ("CRASH Landing")
+    intro.start()
+    intro.how_to_play()
+
+
+
+
+
+
+
+
+
+
+
+#name = input("Enter your name:")
+#print("Hello"  +name+"!")
